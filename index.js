e907660d526458e02cef3d6ca9d7ea01d85e1522
yolovi <yolandalopevi@gmail.com>
2023-05-26 21:31:04 +0200

    first commit ejericios en pareja

---
+++ index.js
@@ -0,0 +1,108 @@
+
+// PROYECTO en PAREJAS -------------
+
+// ejercicio 1 ---------------------
+let variableSinValor;
+
+// ejercicio 2 ---------------------
+let booleano1 = true;
+let booleano2 = true;
+
+// ejercicio 3 ---------------------
+const PI = 3.14;
+
+// ejercicio 4 ---------------------
+const TAU = 2 * PI;
+
+// ejercicio 5 ---------------------
+
+let booleanoAnd = booleano1 && booleano2;
+
+// ejercicio 6 ---------------------
+let booleanoNot = !booleano1;
+
+
+// ejercicio 7 ---------------------
+
+let booleanoMix0 = (booleano1 || booleano2)
+    && (booleano1 || (!booleano1 &&  !booleano2))
+
+// ejercicio 8 ---------------------
+
+let incrementarDesp = 2;
+let resultadoDesp = 0;
+
+resultadoDesp += (incrementarDesp++);
+console.log(resultadoDesp)
+
+// ejercicio 9 ---------------------
+
+let incrementarAntes = 2;
+let resultadoAntes = 0;
+
+resultadoAntes += (++incrementarAntes);
+
+console.log(resultadoAntes)
+
+// ejercicio 10 ---------------------
+let contarHasta10_2 = 0;
+
+for (let i = contarHasta10_2; i <= 10; i++) {
+
+    contarHasta10_2 = i;
+}
+
+// ejercicio 11 ---------------------
+
+let postI = 0;
+let postJ = 0;
+
+for (let i = 0; i < 11; i++) {
+    postI = postI + postJ++
+
+    console.log(postI)
+}
+
+
+// ejercicio 12 ---------------------
+
+let sumaPares = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    if (i % 2 == 0) {
+
+        sumaPares = i + sumaPares
+    }
+}
+
+
+// PROYECTO INDIVIDUAL -------------
+
+// -------------------- Variables -----------------------
+
+// ejercicio 13 --------------------
+ let variableValorNumerico = 7;
+
+// ejercicio 14 --------------------
+const MiNombre = "tu nombre";
+
+// ejercicio 15 --------------------
+ const MiNumeroFav = 3;
+
+// -------------------- Booleanos -----------------------
+
+// - [ ] 16.- Crear variable **booleanoOr** cuyo calor sea la comparación booleana **booleano1 or booleano2**
+
+// let booleanoOr = booleano1 || booleano2
+
+// let booleano1 = true;
+// let booleano2 = false;
+
+// for (let i = 0; index < i.length === 10; i++) {
+    
+//     const contarHasta10_2 = array[contarHasta10_2];
+    
+// }
+
+
